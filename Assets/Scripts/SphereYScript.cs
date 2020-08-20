b8fc2814f26ba99f1d7d51f63ff395c75f5d2459
aavivi <saavivi@campus.technion.ac.il>
2020-08-20 20:55:32 +0300

    Adding project source code

---
+++ Assets/Scripts/SphereYScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereYScript : MonoBehaviour
+{
+    private float sphereYPosition;
+    public GameObject textDisplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphereYPosition = GameObject.Find("Sphere").transform.position.y;
+        textDisplay.GetComponent<Text>().text = "Height: " + sphereYPosition.ToString();
+    }
+}
